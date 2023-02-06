ffe1caf9cbfe98e2f6f091acb6d639a05f4f2eb7
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-06 15:48:43 +0300

    Assignment completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+# greetProgrammer
+  def greet_programmer
+    puts "Hello, programmer!"
+  end
+
+#   greet
+  def greet (name)
+    puts "Hello, #{name}!"
+  end
+  greet("Jimmy")
+
+#   greet with default
+
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!";
+  end
+  greet_with_default
+# sum of 2 numbers
+
+  def add(num1, num2)
+    return num1 + num2
+  end
+  add(1, 1)
+
+#   halve
+\
+  def halve(number)
+    if number.class != Integer
+        return nil
+    end
+        return number / 2
+    end 
+    halve(10)
